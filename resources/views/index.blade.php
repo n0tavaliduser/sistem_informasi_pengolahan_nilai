51decc42ad529f8c99c85442a47fa6e370190c00
NAVU <73369743+n0tavaliduser@users.noreply.github.com>
2023-08-15 23:33:07 +0700

    feat(dashboard): count of jurusan, guru, pelajaran

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,15 +14,98 @@
                 <div class="col-12">
                     <div class="d-flex align-items-lg-center flex-lg-row flex-column">
                         <div class="flex-grow-1">
-                            <h4 class="fs-16 mb-1">Good Morning, {{ Auth::user()->name }}!</h4>
-                            <p class="text-muted mb-0">Here's what's happening with your store
-                                today.</p>
+                            <h4 class="fs-16 mb-1">Halo, {{ Auth::user()->name }}!</h4>
+                            <p class="text-muted mb-0">Data statistik SMK Negeri 1 Maros saat ini</p>
                         </div>
                     </div><!-- end card header -->
                 </div>
                 <!--end col-->
             </div>
             <!--end row-->
+
+            @if (Auth::user()->role->name === 'Admin' || Auth::user()->role->name === 'Kepala Sekolah')
+            <div class="row">
+                <div class="col-xl-4 col-md-6">
+                    <!-- card -->
+                    <div class="card card-animate">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center">
+                                <div class="flex-grow-1 overflow-hidden">
+                                    <p class="text-uppercase fw-medium text-muted text-truncate mb-0">
+                                        Jumlah Jurusan</p>
+                                </div>
+                            </div>
+                            <div class="d-flex align-items-end justify-content-between mt-4">
+                                <div>
+                                    <h4 class="fs-22 fw-semibold ff-secondary mb-4"><span class="counter-value" data-target="{{ \App\Models\Jurusan::count() }}"></span> Jurusan
+                                    </h4>
+                                    @if (Auth::user()->role->name === 'Admin')
+                                    <a href="{{ route('master-data.jurusan.index') }}" class="link-secondary text-decoration-underline">Lihat semua jurusan</a>
+                                    @endif
+                                </div>
+                                <div class="avatar-sm flex-shrink-0">
+                                    <span class="avatar-title bg-soft-primary rounded fs-3">
+                                        <i class="bx bx-dollar-circle text-primary"></i>
+                                    </span>
+                                </div>
+                            </div>
+                        </div><!-- end card body -->
+                    </div><!-- end card -->
+                </div><!-- end col -->
+
+                <div class="col-xl-4 col-md-6">
+                    <!-- card -->
+                    <div class="card card-animate">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center">
+                                <div class="flex-grow-1 overflow-hidden">
+                                    <p class="text-uppercase fw-medium text-muted text-truncate mb-0">Jumlah Guru</p>
+                                </div>
+                            </div>
+                            <div class="d-flex align-items-end justify-content-between mt-4">
+                                <div>
+                                    <h4 class="fs-22 fw-semibold ff-secondary mb-4"><span class="counter-value" data-target="{{ \App\Models\Guru::count() }}"></span> Guru</h4>
+                                    @if (Auth::user()->role->name === 'Admin')
+                                    <a href="{{ route('master-data.guru.index') }}" class="link-secondary text-decoration-underline">Lihat semua guru</a>
+                                    @endif
+                                </div>
+                                <div class="avatar-sm flex-shrink-0">
+                                    <span class="avatar-title bg-soft-primary rounded fs-3">
+                                        <i class="bx bx-shopping-bag text-primary"></i>
+                                    </span>
+                                </div>
+                            </div>
+                        </div><!-- end card body -->
+                    </div><!-- end card -->
+                </div><!-- end col -->
+
+                <div class="col-xl-4 col-md-12">
+                    <!-- card -->
+                    <div class="card card-animate">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center">
+                                <div class="flex-grow-1 overflow-hidden">
+                                    <p class="text-uppercase fw-medium text-muted text-truncate mb-0">Jumlah Mata Pelajaran</p>
+                                </div>
+                            </div>
+                            <div class="d-flex align-items-end justify-content-between mt-4">
+                                <div>
+                                    <h4 class="fs-22 fw-semibold ff-secondary mb-4"><span class="counter-value" data-target="{{ \App\Models\MataPelajaran::count() }}"></span> Mata Pelajaran</h4>
+                                    @if (Auth::user()->role->name === 'Admin')
+                                    <a href="{{ route('master-data.mata-pelajaran.index') }}" class="link-secondary text-decoration-underline">Lihat mata pelajaran</a>
+                                    @endif
+                                </div>
+                                <div class="avatar-sm flex-shrink-0">
+                                    <span class="avatar-title bg-soft-primary rounded fs-3">
+                                        <i class="bx bx-user-circle text-primary"></i>
+                                    </span>
+                                </div>
+                            </div>
+                        </div><!-- end card body -->
+                    </div><!-- end card -->
+                </div><!-- end col -->
+            </div> <!-- end row-->
+            @endif
 
         </div> <!-- end .h-100-->
 
