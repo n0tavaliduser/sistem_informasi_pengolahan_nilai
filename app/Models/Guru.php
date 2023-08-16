094099b464e8938fe692bd9b8ccde178dfeec8a6
NAVU <73369743+n0tavaliduser@users.noreply.github.com>
2023-08-16 22:02:36 +0700

    refactor(models): guru hasfactory

--- app/Models/Guru.php
+++ app/Models/Guru.php
@@ -8,6 +8,7 @@ namespace App\Models;
 
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
@@ -33,6 +34,8 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Guru extends Model
 {
+	use HasFactory;
+
 	protected $table = 'guru';
 
 	protected $casts = [
