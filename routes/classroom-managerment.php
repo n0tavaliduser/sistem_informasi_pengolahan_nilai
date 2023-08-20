2eba0673838aedfd53c0bd613c1f537e75bff5cd
NAVU <73369743+n0tavaliduser@users.noreply.github.com>
2023-08-20 23:19:22 +0700

    Siswa Detail Route

--- routes/classroom-managerment.php
+++ routes/classroom-managerment.php
@@ -7,4 +7,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(Authenticate::class)->prefix('manajemen-kelas')->group(function () {
     Route::get('/daftar-kelas', [RuangKelasController::class, 'daftarKelas'])->name('manajemen-kelas.daftar-kelas');
     Route::get('/ruang-kelas/{jadwal}', [RuangKelasController::class, 'ruangKelas'])->name('manajemen-kelas.ruang-kelas');
+    Route::get('/ruang-kelas/siswa-detail/{siswa}', [RuangKelasController::class, 'detailSiswa'])->name('manajemen-kelas.detail-siswa');
 }); 
