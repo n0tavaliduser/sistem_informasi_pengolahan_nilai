0dc430fc1c94125f129c375e183800cc595102a1
NAVU <73369743+n0tavaliduser@users.noreply.github.com>
2023-08-20 16:46:40 +0700

    Update Mata Pelajaran Seeder

--- database/seeders/MataPelajaranSeeder.php
+++ database/seeders/MataPelajaranSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Jurusan;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -13,28 +12,38 @@ class MataPelajaranSeeder extends Seeder
      */
     public function run()
     {
-        // Get jurusan IDs from the database
-        $jurusanIds = DB::table('jurusan')->orderBy('nama_jurusan', 'ASC')->pluck('id');
-
         // Seed data for the mata_pelajaran table
-        $mata_pelajaran = [
-            'PAI3', 'KEJUR', 'PKn1', 'BIND1', 'PILIH', 'MAT2', 'PAI1', 'BING3', 'BIND4', 'PKn2',
-            'PKK', 'BIND3', 'MAT1', 'BING2', 'BING1', 'PAI2', 'BING4', 'A G M'
+        $mata_pelajaran_kepanjangan = [
+            'PAI3' => 'Pendidikan Agama Islam 3',
+            'KEJUR' => 'Kejuruan',
+            'PKn1' => 'Pendidikan Kewarganegaraan 1',
+            'BIND1' => 'Bahasa Indonesia 1',
+            'PILIH' => 'Mata Pelajaran Pilihan',
+            'MAT2' => 'Matematika 2',
+            'PAI1' => 'Pendidikan Agama Islam 1',
+            'BING3' => 'Bahasa Inggris 3',
+            'BIND4' => 'Bahasa Indonesia 4',
+            'PKn2' => 'Pendidikan Kewarganegaraan 2',
+            'PKK' => 'Pendidikan Keterampilan Khusus',
+            'BIND3' => 'Bahasa Indonesia 3',
+            'MAT1' => 'Matematika 1',
+            'BING2' => 'Bahasa Inggris 2',
+            'BING1' => 'Bahasa Inggris 1',
+            'PAI2' => 'Pendidikan Agama Islam 2',
+            'BING4' => 'Bahasa Inggris 4',
+            'A G M' => 'Agama, Etika, dan Moral'
         ];
 
         $mataPelajaranData = [];
 
-        // foreach ($jurusanIds as $jurusanId) {
-            foreach ($mata_pelajaran as $mata_pelajaran_code) {
-                $mataPelajaranData[] = [
-                    'nama' => 'Mata Pelajaran ' . $mata_pelajaran_code . ' for Jurusan ' . Jurusan::where('id', 1)->value('nama_jurusan'),
-                    'kode' => $mata_pelajaran_code,
-                    'keterangan' => 'Keterangan mata pelajaran ' . $mata_pelajaran_code,
-                    // 'jurusan_id' => $jurusanId,
-                    'jurusan_id' => 1
-                ];
-            }
-        // }
+        foreach ($mata_pelajaran_kepanjangan as $kode => $nama) {
+            $mataPelajaranData[] = [
+                'nama' => $nama,
+                'kode' => $kode,
+                'keterangan' => 'Keterangan mata pelajaran ' . $kode,
+                'jurusan_id' => 1
+            ];
+        }
 
         // Insert the data into the mata_pelajaran table
         DB::table('mata_pelajaran')->insert($mataPelajaranData);
