30120d27809e248eb7185a78e11f42a6fb3be02a
NAVU <73369743+n0tavaliduser@users.noreply.github.com>
2023-08-23 21:12:47 +0700

    Dashboard Grafik Absensi Keseluruhan

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -131,6 +131,19 @@
                     </div><!-- end card -->
                 </div><!-- end col -->
             </div> <!-- end row-->
+
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4 class="card-title mb-0">Grafik Absensi Keseluruhan</h4>
+                        </div>
+                        <div class="card-body">
+                            <canvas id="lineChart" class="chartjs-chart" data-colors='["--vz-primary-rgb, 0.2", "--vz-primary", "--vz-info-rgb, 0.2", "--vz-info"]'></canvas>
+                        </div>
+                    </div>
+                </div> <!-- end col -->
+            </div>
             @endif
 
         </div> <!-- end .h-100-->
@@ -138,9 +151,118 @@
     </div> <!-- end col -->
 </div>
 
+@php
+    use App\Models\Siswa;
+
+    $kehadiranData = [];
+    $alphaData = [];
+    $lastSevenDays = now()->subDays(6)->toDateString(); // 7 days ago
+
+    // Retrieve "Kehadiran" data for each day
+    foreach (range(0, 6) as $daysAgo) {
+        $tanggal = now()->subDays($daysAgo)->toDateString();
+
+        $kehadiranCount = Siswa::with('absensis')
+            ->whereHas('absensis', function ($query) use ($tanggal) {
+                $query->where('tanggal', $tanggal)
+                    ->where('keterangan', 'hadir');
+            })
+            ->count();
+
+        $kehadiranData[] = $kehadiranCount;
+    }
+
+    foreach (range(0, 6) as $daysAgo) {
+        $tanggal = now()->subDays($daysAgo)->toDateString();
+
+        $alphaCount = Siswa::with('absensis')
+            ->whereHas('absensis', function ($query) use ($tanggal) {
+                $query->where('tanggal', $tanggal)
+                    ->where('keterangan', 'alpha');
+            })
+            ->count();
+
+        $alphaData[] = $alphaCount;
+    }
+@endphp
+
 
 @endsection
 @section('script')
+<script src="{{ URL::asset('build/libs/chart.js/chart.min.js') }}"></script>
+<script>
+    // get colors array from the string
+    function getChartColorsArray(chartId) {
+        var chartElement = document.getElementById(chartId);
+        if (chartElement !== null) {
+            var colors = chartElement.getAttribute("data-colors");
+            colors = JSON.parse(colors);
+            return colors.map(function (value) {
+                var newValue = value.replace(" ", "");
+                if (newValue.indexOf(",") === -1) {
+                    var color = getComputedStyle(document.documentElement).getPropertyValue(newValue);
+                    return color.trim() || newValue;
+                } else {
+                    var val = value.split(',');
+                    if (val.length === 2) {
+                        var rgbaColor = getComputedStyle(document.documentElement).getPropertyValue(val[0]);
+                        rgbaColor = "rgba(" + rgbaColor + "," + val[1] + ")";
+                        return rgbaColor;
+                    } else {
+                        return newValue;
+                    }
+                }
+            });
+        }
+        return []; // Return an empty array if the chart element is not found
+    }
+
+    Chart.defaults.borderColor = "rgba(133, 141, 152, 0.1)";
+    Chart.defaults.color = "#858d98";
+
+    // line chart
+    var islinechart = document.getElementById('lineChart');
+    var lineChartColor = getChartColorsArray('lineChart');
+    if (lineChartColor.length > 0) {
+        islinechart.setAttribute("width", islinechart.parentElement.offsetWidth);
+
+        var lineChart = new Chart(islinechart, {
+            type: 'line',
+            data: {
+                labels: {!! json_encode(array_map(fn($i) => now()->subDays($i)->toDateString(), range(0, 6))) !!},
+                datasets: [
+                    {
+                        label: "Kehadiran",
+                        fill: true,
+                        lineTension: 0.5,
+                        backgroundColor: lineChartColor[0],
+                        borderColor: lineChartColor[1],
+                        startFromZero: true,
+                        data: {!! json_encode($kehadiranData) !!}
+                    },
+                    {
+                        label: "Alpha",
+                        fill: true,
+                        lineTension: 0.5,
+                        backgroundColor: lineChartColor[0],
+                        borderColor: lineChartColor[1],
+                        startFromZero: true,
+                        data: {!! json_encode($alphaData) !!}
+                    }
+                ]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    }
+</script>
+
+</script>
 <!-- apexcharts -->
 <script src="{{ URL::asset('build/libs/apexcharts/apexcharts.min.js') }}"></script>
 <script src="{{ URL::asset('build/libs/jsvectormap/js/jsvectormap.min.js') }}"></script>
