893994591c1686a7222252a67d6158076aafcdd4
NAVU <73369743+n0tavaliduser@users.noreply.github.com>
2023-08-27 12:51:31 +0700

    Login page update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,154 +5,74 @@
 @section('content')
 
 <!-- auth-page wrapper -->
-<div class="auth-page-wrapper py-5 d-flex justify-content-center align-items-center min-vh-100"
-    style="
+<div class="auth-page-wrapper py-5 d-flex justify-content-center align-items-center min-vh-100" style="
         background-image: url({{ URL::asset('assets/img/bg1.jpg') }});
         background-position: center;
         background-size: cover;
         background-repeat: no-repeat;
-    "
-    >
+    ">
     <div class="bg-overlay"></div>
     <!-- auth-page content -->
     <div class="auth-page-content overflow-hidden pt-lg-5">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="card overflow-hidden">
-                        <div class="row g-0">
-                            <div class="col-lg-6">
-                                <div class="p-lg-5 p-4 h-100"
-                                style="
-                                    background-image: url({{ URL::asset('assets/img/bg2.jpg') }});
-                                    background-position: center;
-                                    background-size: cover;
-                                    background-repeat: no-repeat;
-                                "
-                                >
-                                    <div class="bg-overlay"></div>
-                                    <div class="position-relative h-100 d-flex flex-column">
-                                        <div class="mb-4">
-                                            <a href="index" class="d-block">
-                                                <img src="{{ URL::asset('build/images/logo-light.png') }}" alt="" height="18">
-                                            </a>
+        <div class="container d-flex flex-column justify-content-center align-items-center">
+            <h2 class="mb-4 text-light">Selamat Datang</h2>
+            <div class="card overflow-hidden w-50 bg-white bg-opacity-50">
+                <div class="row g-0">
+                    <div class="col-lg-12">
+                        <div class="p-lg-5 p-4">
+                            <h3 class="mb-4 text-center text-dark">LOGIN</h3>
+                            <div class="mt-0">
+                                <form action="{{ route('login') }}" method="POST">
+                                    @csrf
+                                    <div class="mb-3">
+                                        <div class="input-group">
+                                            <span class="input-group-text" id="basic-addon1"><i class="ri-shield-user-fill"></i></span>
+                                            <input type="email" id="email" name="email" value="{{ old('email', 'admin@smk.com') }}" class="form-control @error('email') is-invalid @enderror" placeholder="Masukan E-mail" aria-label="email" aria-describedby="basic-addon1">
                                         </div>
-                                        <div class="mt-auto">
-                                            <div class="mb-3">
-                                                <i class="ri-double-quotes-l display-4 text-success"></i>
-                                            </div>
+                                        @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                        @enderror
+                                    </div>
 
-                                            <div id="qoutescarouselIndicators" class="carousel slide" data-bs-ride="carousel">
-                                                <div class="carousel-indicators">
-                                                    <button type="button" data-bs-target="#qoutescarouselIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                                                    <button type="button" data-bs-target="#qoutescarouselIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                                                    <button type="button" data-bs-target="#qoutescarouselIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                                                </div>
-                                                <div class="carousel-inner text-center text-white-50 pb-5">
-                                                    <div class="carousel-item active">
-                                                        <p class="fs-15 fst-italic">" Great! Clean code, clean design,
-                                                            easy for customization. Thanks very much! "</p>
-                                                    </div>
-                                                    <div class="carousel-item">
-                                                        <p class="fs-15 fst-italic">" The theme is really great with an
-                                                            amazing customer support."</p>
-                                                    </div>
-                                                    <div class="carousel-item">
-                                                        <p class="fs-15 fst-italic">" Great! Clean code, clean design,
-                                                            easy for customization. Thanks very much! "</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <!-- end carousel -->
+                                    <div class="mb-3">
+                                        <div class="input-group">
+                                            <span class="input-group-text" id="basic-addon1"><i class="ri-lock-password-fill"></i></span>
+                                            <input type="password" id="password" name="password" value="{{ old('password', 'abcd1234') }}" class="form-control @error('password') is-invalid @enderror" placeholder="Masukan Password" aria-label="password" aria-describedby="basic-addon1">
+                                        </div>
+                                        @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                        <button class="btn btn-link position-absolute end-0 top-0 text-decoration-none text-muted password-addon" type="button" id="password-addon"><i class="ri-eye-fill align-middle"></i></button>
                                         </div>
                                     </div>
-                                </div>
-                            </div>
-                            <!-- end col -->
 
-                            <div class="col-lg-6">
-                                <div class="p-lg-5 p-4">
-                                    <div>
-                                        <h5 class="text-primary">Welcome Back !</h5>
-                                        <p class="text-muted">Sign in to continue to Edu System.</p>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="" id="auth-remember-check">
+                                        <label class="form-check-label" for="auth-remember-check">Remember
+                                            me</label>
                                     </div>
 
                                     <div class="mt-4">
-                                        <form action="{{ route('login') }}" method="POST">
-                                            @csrf
-
-                                            <div class="mb-3">
-                                                <label for="username" class="form-label">Username</label>
-                                                <input type="text" class="form-control @error('email') is-invalid @enderror" value="{{ old('email', 'admin@smk.com') }}" id="username" name="email" placeholder="Enter username">
-                                                @error('email')
-                                                <span class="invalid-feedback" role="alert">
-                                                    <strong>{{ $message }}</strong>
-                                                </span>
-                                                @enderror
-                                            </div>
-
-                                            <div class="mb-3">
-                                                <div class="float-end">
-                                                    <a href="{{ route('password.request') }}" class="text-muted">Forgot
-                                                        password?</a>
-                                                </div>
-                                                <label class="form-label" for="password-input">Password</label>
-                                                <div class="position-relative auth-pass-inputgroup mb-3">
-                                                    <input type="password" class="form-control pe-5 password-input @error('password') is-invalid @enderror" name="password" placeholder="Enter password" id="password-input" value="abcd1234">
-                                                    @error('password')
-                                                    <span class="invalid-feedback" role="alert">
-                                                        <strong>{{ $message }}</strong>
-                                                    </span>
-                                                    @enderror
-                                                    <button class="btn btn-link position-absolute end-0 top-0 text-decoration-none text-muted password-addon" type="button" id="password-addon"><i class="ri-eye-fill align-middle"></i></button>
-                                                </div>
-                                            </div>
-
-                                            <div class="form-check">
-                                                <input class="form-check-input" type="checkbox" value="" id="auth-remember-check">
-                                                <label class="form-check-label" for="auth-remember-check">Remember
-                                                    me</label>
-                                            </div>
-
-                                            <div class="mt-4">
-                                                <button class="btn btn-success w-100" type="submit">Sign In</button>
-                                            </div>
-
-                                        </form>
+                                        <button class="btn btn-success w-100" type="submit">Sign In</button>
                                     </div>
-                                </div>
-                            </div>
-                            <!-- end col -->
-                        </div>
-                        <!-- end row -->
-                    </div>
-                    <!-- end card -->
-                </div>
-                <!-- end col -->
 
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end col -->
+                </div>
+                <!-- end row -->
             </div>
+            <!-- end card -->
             <!-- end row -->
         </div>
         <!-- end container -->
     </div>
-    <!-- end auth page content -->
-
-    <!-- footer -->
-    <footer class="footer">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="text-center">
-                        <script>
-                            document.write(new Date().getFullYear())
-
-                        </script> Sistem Informasi Pengolahan Nilai. Developed <i class="mdi mdi-heart text-danger"></i> by Dian</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
-    <!-- end Footer -->
 </div>
 <!-- end auth-page-wrapper -->
 
